5d48b8794b684986ebbf287e512dc1e2c5ea03e9
노태성 <taesung_no@naver.com>
2025-09-12 16:12:42 +0900

    리스트 연산

---
+++ Basic/ex023.py
@@ -0,0 +1,10 @@
+list1 = [1, 2, 3, 4]
+list2 = [5, 6, 7, 8]
+list3 = list1 + list2
+print(list3)
+print(list1[0] + list2[0], list1[1] + list2[1], list1[2] + list2[2], list1[3] + list2[3])
+list4 = [list1[0] + list2[0], list1[1] + list2[1], list1[2] + list2[2], list1[3] + list2[3]]
+print(list4)
+print(list2 * 3)
+print(len(list1))
+print(len(list3))

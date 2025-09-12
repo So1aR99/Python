1d21f97df71186960d91100b3dee1abc913c420e
노태성 <taesung_no@naver.com>
2025-09-12 14:24:08 +0900

    홀짝 판별

--- Basic/ex016.py
+++ Basic/ex016.py
@@ -8,6 +8,7 @@ if last_num % 2 == 0:
 if last_num % 2 == 1:
     print("홀수")
 
+
 x = input("정수 입력 : ")
 if x[-1] == '0' or x[-1] == '2' or x[-1] == '4' or x[-1] == '6' or x[-1] == '8':
     print("짝수")
@@ -15,8 +16,10 @@ else:
 
     print("홀수")
 
+
 x = input("정수 입력 : ")
 if x[-1] in '02468':
     print("짝수")
 else:
     print("홀수")
+

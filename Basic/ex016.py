0aa84fc79c3ea5ad80e952cfafb102ae0ad12256
노태성 <taesung_no@naver.com>
2025-09-12 14:05:13 +0900

    Update ex016.py

--- Basic/ex016.py
+++ Basic/ex016.py
@@ -12,4 +12,11 @@ x = input("정수 입력 : ")
 if x[-1] == '0' or x[-1] == '2' or x[-1] == '4' or x[-1] == '6' or x[-1] == '8':
     print("짝수")
 else:
+
+    print("홀수")
+
+x = input("정수 입력 : ")
+if x[-1] in '02468':
+    print("짝수")
+else:
     print("홀수")

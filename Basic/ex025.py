ae5f95a9df020ab1acbdf94e30a22a47f71c94a0
노태성 <taesung_no@naver.com>
2025-09-15 10:33:58 +0900

    for 반복문

---
+++ Basic/ex025.py
@@ -0,0 +1,15 @@
+for _ in range(100):
+    print("Hello world")
+
+print(range(100))
+
+for _ in "Helloworld":
+    print("Hello world")
+
+array = [273, 32, 103, 57, 52]
+
+for element in array:
+    print(element)
+
+for character in "안녕하세요":
+    print("-", character)

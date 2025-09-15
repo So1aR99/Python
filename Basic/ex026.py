a3969c6c45342720e05947b05817af111c4b18b5
노태성 <taesung_no@naver.com>
2025-09-15 10:48:09 +0900

    이중 for 반복문

---
+++ Basic/ex026.py
@@ -0,0 +1,17 @@
+list_of_list = [[1, 2, 3], [4, 5, 6], [7, 8 ,9]]
+for i in range(3):
+    for j in range(3):
+        print(list_of_list[i][j], end="  ")
+    print()
+
+list_of_list = [[1, 2, 3], [4, 5, 6, 7], [8 ,9]]
+for i in range(3):
+    for j in range(len(list_of_list[i])):
+        print(list_of_list[i][j], end="  ")
+    print()
+
+list_of_list = [[1, 2, 3], [4, 5, 6, 7], [8 ,9]]
+for i in list_of_list:
+    for j in i:
+        print(j, end="  ")
+    print()

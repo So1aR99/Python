e96de7b7917acd44b7fada62344ac68d2df7199c
노태성 <taesung_no@naver.com>
2025-10-15 09:36:01 +0900

    ramdom 모듈

---
+++ Basic/ex067.py
@@ -0,0 +1,19 @@
+import random
+from pkgutil import resolve_name
+
+print("# random 모듈")
+
+print("- random():", random.random()) # 0.0 <= x < 1.0 사이의 float
+
+print("- uniform(10, 20):", random.uniform(10,20)) # uniform(min, max) : 지정한 범위 사이의 float
+
+# randrange(): 지정한 범위의 int
+# - randrange(max): 0부터 max 사이의 값
+# - randrange(min, max): min부터 max사이의 값
+print("- randrange(10):", random.randrange(10))
+
+print("- choice([1, 2, 3, 4, 5]):", random.choice([1, 2, 3, 4, 5])) # 리스트 내부에 있는 요소 랜덤 선택
+
+print("- shuffle([1, 2, 3, 4, 5]):", random.shuffle([1, 2, 3, 4, 5])) # 리스트의 요소들을 랜덤하게 섞음
+
+print("- sample([1, 2, 3, 4, 5], k=2):", random.sample([1, 2, 3, 4, 5], k=2)) # 리스트의 요소 중 k개를 뽑음
